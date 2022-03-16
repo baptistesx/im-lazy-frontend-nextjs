687498234b49be0cdbf0c67c0a90f603130ee854
Baptiste Seux <seuxbaptiste@gmail.com>
2022-03-16 20:12:38 +0100

    responsive NotSignedInLayout

--- components/layout/NotSignInedLayout.tsx
+++ components/layout/NotSignInedLayout.tsx
@@ -10,36 +10,39 @@ const NotSignedInLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
   return (
-    <Box sx={{ display: "flex", height: "100%" }}>
+    <Box sx={{ display: "flex", height: "100%", flexWrap: "wrap" }}>
       <Box
         sx={{
-          width: "50%",
-          backgroundColor: "white",
+          width: { xs: "100%", sm: "50%" },
+          height: { xs: "20%", sm: "100%" },
+          backgroundColor: { xs: theme.palette.primary.main, sm: "white" },
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          flexWrap: "wrap",
         }}
       >
         <Link href={router.route !== "/" ? "/" : "#"}>
-          <Button sx={{ height: 150 }}>
-            <Image
-              src="/imlazy-logo-dark-400-200.png"
-              height={150}
-              width={300}
-            />
+          <Button sx={{ height: 150, display: { xs: "block", sm: "none" } }}>
+            <Image src="/logo-light.png" height={100} width={200} />
+          </Button>
+          <Button sx={{ height: 150, display: { xs: "none", sm: "block" } }}>
+            <Image src="/logo-dark-400-200.png" height={100} width={200} />
           </Button>
         </Link>
       </Box>
 
       <Box
         sx={{
-          width: "50%",
+          width: { xs: "100%", sm: "50%" },
+          height: { xs: "80%", sm: "100%" },
           backgroundColor: theme.palette.primary.main,
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           rowGap: 5,
+          p: 1,
         }}
       >
         {children}
